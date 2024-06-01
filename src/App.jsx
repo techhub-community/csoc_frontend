6b626d899326a2c7ba4268f840d0860f3b23e71c
Rishabh Kumar <rishabh.kumar.pro@gmail.com>
2024-06-02 00:06:36 +0530

    Added testimonial section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import FAQ from "./components/FAQ"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
+import Testimonial from "./components/Testimonial"
 import Timeline from "./components/Timeline"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Hero />
         <Timeline />
+        <Testimonial />
         <FAQ />
         <ContactUs />
       </main>
