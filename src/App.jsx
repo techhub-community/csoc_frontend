f35c399aceaf3f190d40192eb47b6ebe06b26fac
Rishabh Kumar <rishabh.kumar.pro@gmail.com>
2024-06-01 18:23:07 +0530

    added timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Hero from "./components/Hero"
 import NavBar from "./components/NavBar"
-
+import Timeline from "./components/Timeline"
 
 function App() {
   return (
     <>
       <NavBar />
       <main>
-        <Hero/>
+        <Hero />
+        <Timeline />
       </main>
     </>
   )
