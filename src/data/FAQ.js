0a76d6236caf1b065b39996907fad53051aff06c
Rishabh Kumar <rishabh.kumar.pro@gmail.com>
2024-06-02 00:25:52 +0530

    Moved data for FAQ

---
+++ src/data/FAQ.js
@@ -0,0 +1,26 @@
+export const faqs = [
+  {
+    question: 'What is CodeShack Summer of Code?',
+    answer: 'It is a 2 month long event consisting of two phases - Mentorship Program and Hackathon.',
+  },
+  {
+    question: 'Who all can enroll for this program?',
+    answer: 'All the interested first and second year students can participate in this program.',
+  },
+  {
+    question: 'Which domains will be covered in CSOC?',
+    answer: 'There are three domains - App Dev., Web Dev. and DSA. DSA is compulsory for all the participants registering for this program.',
+  },
+  {
+    question: 'What is the fee for this program?',
+    answer: 'The program is totally free of cost. The only requirement for the program is your consistency and dedication towards learning new things.',
+  },
+  {
+    question: 'Will projects be there?',
+    answer: 'Yes, projects will be there and it will be based on your proficiency level.',
+  },
+  {
+    question: 'What will be the team composition?',
+    answer: 'Each team will have three members. You can register as a solo, duo or trio. Based upon the registrations, we will group the solo and duo participants to form a team of three members.',
+  }
+];
