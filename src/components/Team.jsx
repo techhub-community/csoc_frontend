1eb9f659f1a32942d0e1e5021332ef78b0bdb280
Rishabh Kumar <rishabh.kumar.pro@gmail.com>
2024-06-08 19:19:04 +0530

    Compressed images, added lazy images loading with skimmer & restructured names of images

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import {teamMembers} from '../data/Teams'
-
-
+import { teamMembers } from '../data/Teams';
 
 const ITEMS_PER_PAGE = 6;
 
 const Team = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [imageLoaded, setImageLoaded] = useState({});
+
   const totalPages = Math.ceil(teamMembers.length / ITEMS_PER_PAGE);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
+  };
+
+  const handleImageLoad = (index) => {
+    setImageLoaded((prev) => ({ ...prev, [index]: true }));
   };
 
   const paginatedMembers = teamMembers.slice(
@@ -24,35 +27,38 @@ const Team = () => {
       <h1 className="text-4xl font-bold text-center">Team</h1>
       <p className="text-center text-3xl mt-2 mb-8">Collaboration Is Our Strength!</p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        {paginatedMembers.map((member, index) => (
-          <div key={index} className="text-center">
-            <img
-              src={member.image}
-              alt={member.name}
-              className="rounded-full mx-auto mb-4 w-40 h-40 object-cover"
-            />
-            <h2 className="text-xl font-medium">{member.name}</h2>
-            <div className="mt-2 flex justify-center space-x-4">
-              <a href={member.github} target="_blank" rel="noopener noreferrer">
-                {/* <FaGithub className="text-2xl" /> */}
-              </a>
-              <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
-                {/* <FaLinkedin className="text-2xl" /> */}
-              </a>
+        {
+          paginatedMembers.map((member, index) => (
+            <div key={`${member.name}-${index}`} className="text-center">
+              <div className="relative w-40 h-40 mx-auto mb-4">
+                { !imageLoaded[(currentPage - 1) * ITEMS_PER_PAGE + index] && (
+                  <div className="absolute inset-0 bg-gray-400 animate-pulse rounded-full"></div>
+                )}
+                <img
+                  loading="lazy"
+                  alt={member.name}
+                  src={member.image}
+                  onLoad={() => handleImageLoad((currentPage - 1) * ITEMS_PER_PAGE + index)}
+                  className={`rounded-full object-cover w-40 h-40`}
+                />
+              </div>
+              <h2 className="text-xl font-medium">{member.name}</h2>
             </div>
-          </div>
-        ))}
+          ))
+        }
       </div>
       <div className="mt-8 flex justify-center space-x-4">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            className={`px-4 py-2 rounded ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-            onClick={() => handlePageChange(index + 1)}
-          >
-            {index + 1}
-          </button>
-        ))}
+        {
+          Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={`b-${index}`}
+              className={`px-4 py-2 rounded ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))
+        }
       </div>
     </div>
   );
--- src/data/Testimonial.js
+++ src/data/Testimonial.js
@@ -1,55 +1,55 @@
 export const testimonials = [
   {
-    image: "./image/Manyata.jpg",
+    image: "./image/manyata.jpg",
     qualification: "Web Development",
     name: "Manyata",
     message:
       "It was a great experience attending the CSOC. We learnt many things. Our mentors were very supportive, helping us throughout the journey. It helped us to push ourselves forward in our journey of coding.",
   },
   {
-    image: "./image/Bharath G P.jpg",
+    image: "./image/bharath.jpg",
     qualification: "Web Development",
     name: "Bharath G P",
     message:
       "My journey in the tech team has been transformative, and I am grateful for the opportunities I have had to learn, grow, and collaborate with exceptional professionals.",
   },
   {
-    image: "./image/Pooja.jpeg",
+    image: "./image/pooja.jpeg",
     qualification: "Web Development",
     name: "Pooja Rai",
     message:
       "It was a great experience attending the CSOC 2023. Our mentors were very supportive, helping us throughout the journey. It helped us to push ourselves forward in the journey of coding.",
   },
   {
-    image: "./image/Raghavendra Hande.jpg",
+    image: "./image/raghavendra.jpg",
     qualification: "App Development",
     name: "Raghavendra Hande",
     message:
       "CSOC 2023 was an amazing experience. Got amazing mentors who gave my coding journey the boost that it needed.",
   },
   {
-    image: "./image/Carol Dsilva.jpeg",
+    image: "./image/carol.jpeg",
     qualification: "App Development",
     name: "Carol Dsilva",
     message:
       'It was a wonderful experience gaining hands-on knowledge. It made me feel like a "coder" for the first time. I explored the basics of web dev and came to realize that tech is an ever-evolving field; there is always a next step. The mentors allowed me to progress at my own pace and were always ready to address any doubts I had!',
   },
   {
-    image: "./image/Mohammed Sufyan B.jpg",
+    image: "./image/sufyan.jpg",
     qualification: "DSA",
     name: "Mohammed Sufyan B",
     message:
       "This CSOC sparked a significant interest in technology. The supportive environment fostered by the instructors and peers provided invaluable guidance, which equipped me to begin my tech journey on a strong foundation. It undoubtedly enhanced my overall development in the field.",
   },
   {
-    image: "./image/Saurav.jpg",
+    image: "./image/saurav.jpg",
     qualification: "Web Development",
     name: "Saurabh Singh",
     message:
       "It was really an amazing experience in CSOC. I learned so many new things and explored web development, but I quit at the end. However, I really liked this experience and after CSOC, I believed in myself and started once again in web development, focusing on the frontend part. I completed up to JS.",
   },
   {
-    image: "./image/Prajna Hegde.jpg",
+    image: "./image/prajna.jpg",
     qualification: "UI/UX",
     name: "Prajna Hegde",
     message:
