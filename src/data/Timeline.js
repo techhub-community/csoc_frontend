b3cd37a72ed9c8882d9a55a945489154309c62ce
sweatypenguin624 <sweatypenguin624@gmail.com>
2026-04-19 01:16:44 +0530

    updated timeline

--- src/data/Timeline.js
+++ src/data/Timeline.js
@@ -1,32 +1,32 @@
 export const timelineData = [
-    {
-      date: "12th April 2025",
-      title: "Registration Begins",
-      details: `At 12:00 PM, the registration form will go live. It must be filled completely to be a part of the program.`,
-    },
-    {
-      date: "16th April 2025",
-      title: "Registration Ends",
-      details: `At 11:59 PM, the form will become inactive and no more entries will be entertained. Registered Participants will be notified regarding the further events.`,
-    },
-    {
-      date: "16th April 2025",
-      title: "Opening Ceremony",
-      details: `This will be the kickstart of the program where the mentors will be introduced and the participants can interact with them for any questions.`,
-    },
-    {
-      date: "17th April 2025",
-      title: "Phase 1 Begins",
-      details: `It will be the mentorship phase where participants will be assigned mentors. During this phase, participants will have to complete and submit the tasks given to them within the stipulated timeframe.`,
-    },
-    {
-      date: "14th May 2025",
-      title: "Phase 1 Ends",
-      details: `By the end, the participants would have got extensive preparation and experience in their respective domains, ensuring that they are ready to work on some good projects.`,
-    },
-    {
-      date: "?? 2025",
-      title: "Phase 2 ( Locked )",
-      details: `Will be communicated soon...`,
-    },
-  ];
+  {
+    date: "19th April 2026",
+    title: "Registration Begins",
+    details: `At 12:00 PM, the registration form will go live. It must be filled completely to be a part of the program.`,
+  },
+  {
+    date: "21st April 2026",
+    title: "Registration Ends",
+    details: `At 11:59 PM, the form will become inactive and no more entries will be entertained. Registered Participants will be notified regarding the further events.`,
+  },
+  {
+    date: "22nd April 2026",
+    title: "Opening Ceremony",
+    details: `This will be the kickstart of the program where the mentors will be introduced and the participants can interact with them for any questions.`,
+  },
+  {
+    date: "23rd April 2026",
+    title: "Phase 1 Begins",
+    details: `It will be the mentorship phase where participants will be assigned mentors. During this phase, participants will have to complete and submit the tasks given to them within the stipulated timeframe.`,
+  },
+  {
+    date: "20th May 2026",
+    title: "Phase 1 Ends",
+    details: `By the end, the participants would have got extensive preparation and experience in their respective domains, ensuring that they are ready to work on some good projects.`,
+  },
+  {
+    date: "?? 2026",
+    title: "Phase 2 ( Locked )",
+    details: `Will be communicated soon...`,
+  },
+];
