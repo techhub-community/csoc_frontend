36216ccb518da726402ab571d977b93c6c0eabbe
Avinash <avinashunita@gmail.com>
2026-04-20 18:41:45 +0530

    backend updated

--- src/data/consts.js
+++ src/data/consts.js
@@ -1,2 +1,2 @@
 export const baseUrl = window.location.protocol === "https:"
-  ? "https://csoc-api.040203.xyz" : "http://127.0.0.1:8787";
+  ? "https://csoc_backend.avinashpal24013.workers.dev" : "http://127.0.0.1:8787";
