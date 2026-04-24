a78619471a6a341c4682837ac9b9549c9ee0cd3c
sweatypenguin624 <sweatypenguin624@gmail.com>
2026-04-24 19:09:54 +0530

    updated sessions

--- src/data/sessions.js
+++ src/data/sessions.js
@@ -2,7 +2,7 @@ export const sessionsData = [
   {
     id: 1,
     title: "Git and Version Control",
-    date: "23 April",
+    date: "24 April",
     day: "Thursday",
     time: "9:00 PM",
     mentor: "Avinash Singh Pal | Abhay Kumar",
